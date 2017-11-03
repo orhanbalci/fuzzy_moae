1c3004e5217fd496721e3167657ec76e7a26d2db
orhanbalci <orhanbalci@gmail.com>
2017-11-03 21:22:51 +0300

    Nutrient Group entity added

---
+++ src/main/java/net/orhanbalci/fuzzymoea/entity/NutrientGroup.java
@@ -0,0 +1,31 @@
+package net.orhanbalci.fuzzymoea.entity;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "nutrient_group")
+public class NutrientGroup {
+  @DatabaseField(columnName = "id")
+  private int id;
+
+  @DatabaseField(columnName = "name")
+  private String name;
+
+  public NutrientGroup() {}
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
